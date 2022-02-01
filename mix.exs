ce35f87d3e48133ba2e52662b9a48e39a2145438
Marcus Kruse <esurk.sucram@gmail.com>
2022-02-01 20:42:44 +0100

    Add deps

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule Recode.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/hrzndhrn/recode"
+
   def project do
     [
       app: :recode,
       version: "0.1.0",
       elixir: "~> 1.10",
+      name: "Recode",
+      description: description(),
+      docs: docs(),
+      source_url: @source_url,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      dialyzer: dialyzer(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: preferred_cli_env(),
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -17,10 +28,51 @@ defmodule Recode.MixProject do
     ]
   end
 
+  defp description do
+    "A codre refactoring tool and credo issue solver"
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      formatters: ["html"]
+    ]
+  end
+
+  def preferred_cli_env do
+    [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test,
+      "coveralls.github": :test
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "test/support/plts/dialyzer.plt"},
+      flags: [:unmatched_returns]
+    ]
+  end
+
   defp deps do
     [
-      {:sourceror, git: "https://github.com/doorgan/sourceror"}
-      # {:sourceror, "~> 0.8"}
+      # {:sourceror, "~> 0.9"},
+      {:sourceror, path: "../../forks/sourceror"},
+      {:credo, "~> 1.6", runtime: false},
+      # dev and test
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Marcus Kruse"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
