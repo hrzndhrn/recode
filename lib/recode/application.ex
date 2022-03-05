f33584b4a7b513712630a7288a0fdf6e51bb71eb
Marcus Kruse <esurk.sucram@gmail.com>
2022-03-05 07:37:40 +0100

    Add application

---
+++ lib/recode/application.ex
@@ -0,0 +1,16 @@
+defmodule Recode.Application do
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: MyApp.Worker.start_link(arg)
+      # {MyApp.Worker, arg}
+    ]
+
+    opts = [strategy: :one_for_one, name: MyApp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
