8635e28659533a2e8825e95c918df00bbe33be3c
Marcus Kruse <esurk.sucram@gmail.com>
2022-03-05 08:31:00 +0100

    Add mix rename test

---
+++ test/mix/tasks/recode/rename_test.exs
@@ -0,0 +1,20 @@
+defmodule Mix.Tasks.Recode.RenameTest do
+  use ExUnit.Case
+
+  import Mox
+
+  alias Mix.Tasks.Recode.Rename, as: Task
+  alias Recode.RunnerMock
+
+  setup :verify_on_exit!
+
+  test "mix recode.rename Rename.Bar.baz bar" do
+    expect(RunnerMock, :run, fn {task, opts}, config ->
+      assert task == Recode.Task.Rename
+      assert opts == [from: {Rename.Bar, :baz, nil}, to: %{fun: :bar}]
+      assert config ==  [inputs: "{lib,test}/**/*.{ex,exs}"]
+    end)
+
+    Task.run(["Rename.Bar.baz", "bar"])
+  end
+end
