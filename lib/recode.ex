1fe0dc7b44a814e7dca0bb83112a9909cec1f448
Marcus Kruse <esurk.sucram@gmail.com>
2022-03-15 20:09:59 +0100

    Remove debug code

--- lib/recode.ex
+++ lib/recode.ex
@@ -2,36 +2,4 @@ defmodule Recode do
   @moduledoc """
   A linter with autocorrection and a refactoring tool.
   """
-  left =
-    {:ok,
-     %{
-       attributes: [],
-       compile_opts: [],
-       definitions: [{{:bar, 0}, :def, [line: 2], [{[line: 2], [], [], :bar}]}],
-       deprecated: [],
-       file: "nofile",
-       is_behaviour: false,
-       line: 1,
-       module: Bar,
-       relative_file: "nofile",
-       unreachable: []
-     }}
-
-  right =
-    {:ok,
-     %{
-       attributes: [],
-       compile_opts: [],
-       definitions: [
-         {{:bar, 0}, :def, [line: 2], [{[line: 2], [], [], :bar}]}
-       ],
-       deprecated: [],
-       file: "nofile",
-       is_behaviour: false,
-       line: 1,
-       module: Bar,
-       relative_file: "nofile",
-       struct: nil,
-       unreachable: []
-     }}
 end
