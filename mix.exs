780e88c9030928df9b9a3d74a1a21fd06f5f5f6a
Marcus Kruse <esurk.sucram@gmail.com>
2022-10-15 07:10:23 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -86,7 +86,7 @@ defmodule Recode.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.25", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.10", only: :test},
+      {:excoveralls, "~> 0.15", only: :test},
       {:mox, "~> 1.0", only: :test}
     ]
   end
