35be9f98d582db8e604f4db843fff20426fad371
antedeguemon <antedeguemon@users.noreply.github.com>
2022-10-25 19:12:13 -0400

    Adds my custom configs

---
+++ .recode.exs
@@ -0,0 +1,30 @@
+alias Recode.Task
+
+[
+  version: "0.4.0",
+  # Can also be set/reset with "--autocorrect"/"--no-autocorrect".
+  autocorrect: true,
+  # With "--dry" no changes will be written to the files.
+  # Can also be set/reset with "--dry"/"--no-dry".
+  # If dry is true then verbose is also active.
+  dry: false,
+  # Can also be set/reset with "--verbose"/"--no-verbose".
+  verbose: false,
+  # Can be overwriten by calling `mix recode "lib/**/*.ex"`.
+  inputs: ["{config,lib,test}/**/*.{ex,exs}"],
+  formatter: {Recode.Formatter, []},
+  tasks: [
+    # Tasks could be added by a tuple of the tasks module name and an options
+    # keyword list. A task can be deactived by `active: false`. The execution of
+    # a deactivated task can be forced by calling `mix recode --task ModuleName`.
+    {Task.AliasExpansion, []},
+    {Task.AliasOrder, []},
+    {Task.EnforceLineLength, []},
+    {Task.KeysOrder, []},
+    {Task.PipeFunOne, []},
+    {Task.SinglePipe, []},
+    {Task.Specs, exclude: "test/**/*.{ex,exs}", config: [only: :visible]},
+    {Task.TestFileExt, []},
+    {Task.UnusedVariable, active: false}
+  ]
+]
