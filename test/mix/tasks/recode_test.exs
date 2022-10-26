ebd0dbcfa740cc3c7029fc21786c0662bd0f927c
antedeguemon <antedeguemon@users.noreply.github.com>
2022-10-26 13:26:32 -0400

    Fixes multiple input files handling

--- test/mix/tasks/recode_test.exs
+++ test/mix/tasks/recode_test.exs
@@ -33,12 +33,24 @@ defmodule Mix.Tasks.RecodeTest do
   test "mix recode --config priv/config.exs -" do
     expect(RunnerMock, :run, fn config ->
       assert Keyword.keyword?(config)
-      assert config[:inputs] == "-"
+      assert config[:inputs] == ["-"]
 
       ":test" |> source() |> project()
     end)
 
     assert catch_exit(Tasks.Recode.run(["--config", "priv/config.exs", "-"])) == :normal
+  end
+
+  test "mix recode file_1.ex file_2.ex" do
+    expect(RunnerMock, :run, fn config ->
+      assert Keyword.keyword?(config)
+      assert config[:inputs] == ["file_1.ex", "file_2.ex"]
+
+      ":test" |> source() |> project()
+    end)
+
+    assert catch_exit(Tasks.Recode.run(["--config", "priv/config.exs", "file_1.ex", "file_2.ex"])) ==
+             :normal
   end
 
   test "mix recode raises exception for unknown config file" do
@@ -48,8 +60,8 @@ defmodule Mix.Tasks.RecodeTest do
   end
 
   test "mix recode raises exception for unknown arg" do
-    assert_raise Mix.Error, ~s|["inputs", "foo"] : Unknown|, fn ->
-      Tasks.Recode.run(["--config", "priv/config.exs", "inputs", "foo"])
+    assert_raise OptionParser.ParseError, ~r|unknown-arg : Unknown option|, fn ->
+      Tasks.Recode.run(["--config", "priv/config.exs", "--unknown-arg", "inputs", "foo"])
     end
   end
 
