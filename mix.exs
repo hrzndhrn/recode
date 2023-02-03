ab1aa9d5987abd1885dc55f909850047f45302f0
Marcus Kruse <esurk.sucram@gmail.com>
2023-02-03 16:52:02 +0100

    Fix deps

--- mix.exs
+++ mix.exs
@@ -82,7 +82,7 @@ defmodule Recode.MixProject do
     [
       {:bunt, "~> 0.2"},
       {:glob_ex, "~> 0.1"},
-      {:rewrite, path: "../rewrite"},
+      {:rewrite, "~> 0.4"},
       # dev/test
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
