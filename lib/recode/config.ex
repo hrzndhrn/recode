c67af0064b8fa34fd277ae8623f237caee54e1ec
Marcus Kruse <esurk.sucram@gmail.com>
2023-09-15 11:42:57 +0200

    Fix test

--- lib/recode/config.ex
+++ lib/recode/config.ex
@@ -9,7 +9,7 @@ defmodule Recode.Config do
 
   @config_filename ".recode.exs"
 
-  @config_version "0.6.3"
+  @config_version "0.6.4"
 
   # The minimum version of the config to run recode. This version marks the last
   # breaking change for handle the config.
