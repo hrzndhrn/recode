eb2a5ec86dd3bdccca48d63d10eba0573be592a5
Marcus Kruse <esurk.sucram@gmail.com>
2024-03-16 07:38:46 +0100

    Allow optional config for Recode.Task.Format

--- lib/recode/task/format.ex
+++ lib/recode/task/format.ex
@@ -13,24 +13,77 @@ defmodule Recode.Task.Format do
   alias Recode.Task.Format
   alias Rewrite.Source
 
+  @default_config [formatter: :sourceror]
+  @valid_formatters [:elixir, :sourceror]
+
   @impl Recode.Task
   def run(source, opts) do
     source
     |> Source.Ex.merge_formatter_opts(exclude_plugins: [Recode.FormatterPlugin])
-    |> format(opts[:autocorrect])
+    |> execute(opts[:autocorrect], opts[:formatter])
   end
 
-  defp format(source, true) do
-    Source.update(source, Format, :content, Source.Ex.format(source))
+  defp execute(source, true, formatter) do
+    Source.update(source, Format, :content, format(source, formatter))
   end
 
-  defp format(source, false) do
-    case Source.get(source, :content) == Source.Ex.format(source) do
+  defp execute(source, false, formatter) do
+    case Source.get(source, :content) == format(source, formatter) do
       true ->
         source
 
       false ->
         Source.add_issue(source, Issue.new(Format, "The file is not formatted."))
+    end
+  end
+
+  defp format(source, :sourceror) do
+    Source.Ex.format(source)
+  end
+
+  defp format(source, :elixir) do
+    opts = Keyword.get(source.filetype.opts, :formatter_opts, [])
+
+    source
+    |> Source.get(:content)
+    |> Code.format_string!(opts)
+    |> IO.iodata_to_binary()
+  end
+
+  @impl Recode.Task
+  def init([]), do: {:ok, @default_config}
+
+  def init(config) do
+    with {:error, reason} <- validate(config) do
+      case reason do
+        {:unknown, keys} ->
+          {:error,
+           """
+           Unknown options: #{inspect(keys)}.\
+           """}
+
+        {:invalid_formatter, formatter} ->
+          {:error,
+           """
+           The option formatter expects :elixir or :sourceror, got: #{inspect(formatter)}.\
+           """}
+      end
+    end
+  end
+
+  defp validate(config) do
+    with {:ok, config} <- validate_keys(config) do
+      if config[:formatter] in @valid_formatters do
+        {:ok, config}
+      else
+        {:error, {:invalid_formatter, config[:formatter]}}
+      end
+    end
+  end
+
+  defp validate_keys(config) do
+    with {:error, keys} <- Keyword.validate(config, @default_config) do
+      {:error, {:unknown, keys}}
     end
   end
 end
