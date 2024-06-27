541c9be8c0a738a8a0217cb525ca1c937da94735
Andrew Haust <andrew@hau.st>
2024-06-26 21:04:52 -0400

    Add some test cases

--- test/recode/task/comparisons_test.exs
+++ test/recode/task/comparisons_test.exs
@@ -21,5 +21,41 @@ defmodule Recode.Task.ComparisonsTest do
       |> run_task(Comparisons, autocorrect: true)
       |> assert_code(expected)
     end
+
+    test "greater than" do
+      code = """
+      if foo > bar do
+        true
+      else
+        false
+      end
+      """
+
+      expected = """
+      foo > bar
+      """
+
+      code
+      |> run_task(Comparisons, autocorrect: true)
+      |> assert_code(expected)
+    end
+
+    test "predicate" do
+      code = """
+      if foo?(bar) do
+        true
+      else
+        false
+      end
+      """
+
+      expected = """
+      foo?(bar)
+      """
+
+      code
+      |> run_task(Comparisons, autocorrect: true)
+      |> assert_code(expected)
+    end
   end
 end
