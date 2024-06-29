2f2c0f7eac9dfe670e1cd2c7178f318371e43c04
Andrew Haust <andrew@hau.st>
2024-06-29 18:55:18 -0400

    Remove parens

---
+++ lib/recode/task/remove_parens.ex
@@ -0,0 +1,66 @@
+defmodule Recode.Task.RemoveParens do
+  @shortdoc "Removes parens from locals without parens"
+
+  @moduledoc """
+  Redudant booleans make code needlesly verbose.
+
+          # preferred
+          foo == bar
+
+          # not preferred
+          if foo == bar do
+            true
+          else
+            false
+          end
+
+  This task rewrites the code when `mix recode` runs with `autocorrect: true`.
+  """
+
+  use Recode.Task, corrector: true, category: :readability
+
+  # alias Recode.Issue
+  alias Recode.Task.UnnecessaryIfUnless
+  alias Rewrite.Source
+  alias Sourceror.Zipper
+
+  @impl Recode.Task
+  def run(source, opts) do
+    formatter_opts = Mix.Tasks.Format.formatter_opts_for_file(".mix.exs")
+    locals_without_parens = Keyword.get(formatter_opts, :locals_without_parens, [])
+
+    {zipper, issues} =
+      source
+      |> Source.get(:quoted)
+      |> Zipper.zip()
+      |> Zipper.traverse([], fn zipper, issues ->
+        remove_parens(locals_without_parens, zipper, issues, opts[:autocorrect])
+      end)
+
+    case opts[:autocorrect] do
+      true ->
+        Source.update(source, UnnecessaryIfUnless, :quoted, Zipper.root(zipper))
+
+      false ->
+        Source.add_issues(source, issues)
+    end
+  end
+
+  defp remove_parens(locals_without_parens, %Zipper{} = zipper, issues, true) do
+    node =
+      with {fun, _, args} = node <- Zipper.node(zipper) do
+        Enum.reduce(locals_without_parens, node, fn
+          {^fun, arity}, node when length(args) == arity ->
+            {fun, meta, args} = node
+            meta = Keyword.delete(meta, :closing)
+
+            {fun, meta, args}
+
+          _, node ->
+            node
+        end)
+      end
+
+    {%Zipper{zipper | node: node}, issues}
+  end
+end
---
+++ test/recode/task/remove_parens_test.exs
@@ -0,0 +1,33 @@
+defmodule Recode.Task.RemoveParensTest do
+  use RecodeCase
+
+  alias Recode.Task.RemoveParens
+
+  test "removes parens" do
+    code = """
+    assert(true == false)
+    """
+
+    expected = """
+    assert true == false
+    """
+
+    code
+    |> run_task(RemoveParens, autocorrect: true)
+    |> assert_code(expected)
+  end
+
+  test "respects arity" do
+    code = """
+    assert(true, "assert/2 is not in locals_without_parens")
+    """
+
+    expected = """
+    assert(true, "assert/2 is not in locals_without_parens")
+    """
+
+    code
+    |> run_task(RemoveParens, autocorrect: true)
+    |> assert_code(expected)
+  end
+end
