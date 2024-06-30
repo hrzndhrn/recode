bec3d1d485c4e4ac44f0073fdfe1bfbb6b9ec6de
Andrew Haust <andrew@hau.st>
2024-06-30 08:51:06 -0400

    Update test/recode/task/remove_parens_test.exs
    Co-authored-by: Marcus Kruse <esurk.sucram@gmail.com>

--- test/recode/task/remove_parens_test.exs
+++ test/recode/task/remove_parens_test.exs
@@ -1,7 +1,38 @@
 defmodule Recode.Task.RemoveParensTest do
-  use RecodeCase
+  use RecodeCase, async: true
 
   alias Recode.Task.RemoveParens
+
+  @moduletag :tmp_dir
+
+  setup context do
+    File.write("#{context.tmp_dir}/.formatter.exs", """
+    [
+      inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+      locals_without_parens: [foo: 1, bar: 2]
+    ]
+    """)
+
+    {:ok, context}
+  end
+
+  test "remove parens", %{tmp_dir: tmp_dir} do
+    File.cd!(tmp_dir, fn ->
+      code = """
+      x = foo(bar(y))
+      bar(y, x)
+      """
+
+      expected = """
+      x = foo bar(y)
+      bar y, x
+      """
+
+      code
+      |> run_task(RemoveParens, autocorrect: true)
+      |> assert_code(expected)
+    end)
+  end
 
   test "removes parens" do
     code = """
