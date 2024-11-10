c8875efdea272685ba84893b52c6c3ee033c45f1
Marcus Kruse <esurk.sucram@gmail.com>
2024-11-10 14:42:12 +0100

    Update rewrite

--- mix.exs
+++ mix.exs
@@ -102,8 +102,7 @@ defmodule Recode.MixProject do
     [
       {:escape, "~> 0.1"},
       {:glob_ex, "~> 0.1"},
-      # {:rewrite, "~> 1,1"},
-      {:rewrite, path: "../rewrite/"},
+      {:rewrite, "~> 1.1"},
       # dev/test
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
