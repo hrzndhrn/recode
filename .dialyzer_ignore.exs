417ebaa4975e4835f4a25911219e4e882d868174
Marcus Kruse <esurk.sucram@gmail.com>
2024-11-10 15:02:44 +0100

    Update .dialyzer_ignore.exs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,5 @@
 [
   ~r|lib/mix/tasks/recode.ex:.*:no_return|,
-  ~r|lib/recode/runner/impl.ex:235:apply|,
-  ~r|lib/recode/runner/impl.ex:240:call|
+  ~r|lib/recode/runner/impl.ex:298:apply|,
+  ~r|lib/recode/runner/impl.ex:303:call|
 ]
