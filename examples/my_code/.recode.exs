4171691c9644b698e9b2c865dbc482be978d41e4
Marcus Kruse <esurk.sucram@gmail.com>
2024-11-10 15:19:51 +0100

    Read inputs from .formatter.exs (#98)

--- examples/my_code/.recode.exs
+++ examples/my_code/.recode.exs
@@ -10,8 +10,11 @@
   color: true,
   # Can also be set/reset with `--verbose`/`--no-verbose`.
   verbose: false,
+  # Inputs can be a path, glob expression or list of paths and glob expressions.
+  # With the atom :formatter the inputs from .formatter.exs are
+  # used. also allowed in the list mentioned above.
   # Can be overwritten by calling `mix recode "lib/**/*.ex"`.
-  inputs: ["{mix,.formatter}.exs", "{apps,config,lib,test,priv}/**/*.{ex,exs}"],
+  inputs: :formatter,
   formatters: [Recode.CLIFormatter],
   tasks: [
     # Tasks could be added by a tuple of the tasks module name and an options
