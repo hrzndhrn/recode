87f031a883a044f1800fe11cc8fa42a9c0bd6948
Marcus Kruse <esurk.sucram@gmail.com>
2024-11-16 07:20:08 +0100

    Add specs and docs

--- lib/recode/timestamp.ex
+++ lib/recode/timestamp.ex
@@ -1,6 +1,12 @@
 defmodule Recode.Timestamp do
   @moduledoc false
 
+  # A helper for timestamps.
+  #
+  # `for_file/1` returns `0` for a non-existing file and for files where
+  # `stat/2` returns another error tuple.
+
+  @spec for_file(Path.t()) :: integer()
   def for_file(path) do
     case File.stat(path, time: :posix) do
       {:ok, %{mtime: timestamp}} -> timestamp
