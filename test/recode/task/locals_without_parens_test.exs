a53d9545eaac531736985533559c9426947930fb
Marcus Kruse <esurk.sucram@gmail.com>
2025-01-19 12:37:54 +0100

    Fix LocalsWithoutParens task (#104)

--- test/recode/task/locals_without_parens_test.exs
+++ test/recode/task/locals_without_parens_test.exs
@@ -31,6 +31,35 @@ defmodule Recode.Task.LocalsWithoutParensTest do
     |> assert_code(expected)
   end
 
+  test "does not remove parens in def*" do
+    dot_formatter =
+      DotFormatter.from_formatter_opts(locals_without_parens: [foo: 1, bar: 2, baz: :*])
+
+    code = """
+    def foo(x) do
+      bar(x, 2)
+    end
+
+    def bar(_x, y) do
+      foo(y)
+    end
+    """
+
+    expected = """
+    def foo(x) do
+      bar x, 2
+    end
+
+    def bar(_x, y) do
+      foo y
+    end
+    """
+
+    code
+    |> run_task(LocalsWithoutParens, autocorrect: true, dot_formatter: dot_formatter)
+    |> assert_code(expected)
+  end
+
   test "adds issue" do
     dot_formatter = DotFormatter.from_formatter_opts(locals_without_parens: [foo: 1])
 
@@ -51,13 +80,35 @@ defmodule Recode.Task.LocalsWithoutParensTest do
     |> refute_issues()
   end
 
-  test "adds no issue for dev" do
+  test "adds no issue for def" do
     """
     def hello do
       :world
     end
     """
     |> run_task(LocalsWithoutParens, autocorrect: false)
+    |> refute_issues()
+  end
+
+  test "add no issue for parens in do:" do
+    dot_formatter = DotFormatter.from_formatter_opts(locals_without_parens: [foo: 1])
+
+    """
+    def bar(x) when is_binary(x), do: foo(x)
+    """
+    |> run_task(LocalsWithoutParens, autocorrect: false, dot_formatter: dot_formatter)
+    |> refute_issues()
+  end
+
+  test "add no issue for parens in tuples and maps" do
+    dot_formatter = DotFormatter.from_formatter_opts(locals_without_parens: [foo: 1])
+
+    """
+    {a, foo(b)}
+    {a, foo(b), c}
+    %{a: foo b}
+    """
+    |> run_task(LocalsWithoutParens, autocorrect: false, dot_formatter: dot_formatter)
     |> refute_issues()
   end
 end
