83acdb6f800c9d139e6d5bdb67ac5076fc3c9e24
Marcus Kruse <esurk.sucram@gmail.com>
2025-09-30 20:32:47 +0200

    Raise error in format task

--- lib/recode/task/format.ex
+++ lib/recode/task/format.ex
@@ -56,9 +56,9 @@ defmodule Recode.Task.Format do
     formatter_opts = Keyword.get(source.filetype.opts, :formatter_opts, [])
 
     current_formatter_opts =
-      case DotFormatter.read(ignore_missing_sub_formatters: true) do
+      case DotFormatter.read(ignore_missing_sub_formatters: true) |> IO.inspect() do
         {:ok, dot_formatter} -> DotFormatter.formatter_opts(dot_formatter)
-        {:error, _reason} -> []
+        {:error, reason} -> raise reason
       end
 
     merged_formatter_opts = Keyword.merge(current_formatter_opts, formatter_opts)
